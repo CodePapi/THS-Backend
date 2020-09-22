da47b074073a23f6ee04b5d874edd9c029ea14e6
Samuel Egbajie (MAJ0R) <49847624+CodePapi@users.noreply.github.com>
2020-09-22 04:09:09 +0100

    first push

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+// set up express
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+// set up mongoose
+let uri="mongodb+srv://major:major@cluster0.el0eg.gcp.mongodb.net/major?retryWrites=true&w=majority"
+mongoose.connect(
+  // process.env.MONGODB_CONNECTION_STRING,
+uri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+  }
+);
+
+// set up routes
+
+app.use("/users", require("./routes/userRouter"));
