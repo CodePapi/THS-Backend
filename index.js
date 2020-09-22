f3a0e0cafef1741396ff62200c53813061fc3a7f
Samuel Egbajie (MAJ0R) <49847624+CodePapi@users.noreply.github.com>
2020-09-22 15:38:26 +0100

    push heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-
+let port = process.env.PORT || 5000
 // set up express
 
 const app = express();
